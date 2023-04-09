3860bff04c61b3c636c6b3ddaa3a34ddf0b74362
MukhtarovTimerlan <90184613+MukhtarovTimerlan@users.noreply.github.com>
2023-04-09 19:45:01 +0300

    Add project files.

---
+++ TODOLIST/Models/Request.cs
@@ -0,0 +1,31 @@
+﻿namespace TODOLIST.Models
+{
+    public class Request
+    {
+        public int id { get; set; }
+       
+        public int PersonId { get; set; }
+
+        public RequestData RequestData { get; set; }
+
+        public Request ()
+        {
+
+        }
+
+        public Request(int id, string Title, string Description, int PersonId, int StatusId)
+        {
+            this.id = id;
+            this.RequestData = new RequestData
+            {
+                id = id,
+                Title = Title,
+                Description = Description,
+                StatusId = StatusId,
+                AddData = DateTime.Now,
+                ChangeData = DateTime.Now
+            };
+            this.PersonId = PersonId;
+        }
+    }
+}
